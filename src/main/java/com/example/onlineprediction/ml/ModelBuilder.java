ff7b9286c1b86700f2d8c54d613a9d39300daed2
Darshan Modh <darshanmodh@gmail.com>
2017-10-30 12:16:04 -0700

    Train and test the data
    - category encoder
    - numeric encoder

--- src/main/java/com/example/onlineprediction/ml/ModelBuilder.java
+++ src/main/java/com/example/onlineprediction/ml/ModelBuilder.java
@@ -1,6 +1,8 @@
 package com.example.onlineprediction.ml;
 
 import com.example.onlineprediction.data.DataLoader;
+import org.apache.mahout.classifier.evaluation.Auc;
+import org.apache.mahout.classifier.sgd.L1;
 import org.apache.mahout.classifier.sgd.OnlineLogisticRegression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,8 +24,8 @@ public class ModelBuilder {
         train(PATH);
     }
 
-    public static Map<String, List<OnlineLogisticRegression>> train(String path) throws IOException {
-        final Map<String, List<OnlineLogisticRegression>> regressionMap = new HashMap<>();
+    public static Map<String, OnlineLogisticRegression> train(String path) throws IOException {
+        final Map<String, OnlineLogisticRegression> regressionMap = new HashMap<>();
 
         final Map<String, List<String>> data = DataLoader.getFlightDataByAirport(path);
 
@@ -31,9 +33,47 @@ public class ModelBuilder {
 
         final List<FlightData> flightData = new ArrayList<>();
 
-        // TODO: train the data
+        for(Map.Entry<String, List<String>> entry : sample.entrySet()) {
+            List<String> airportValues = entry.getValue();
+            int trainIndex = (int) (airportValues.size() * TRAINING_PERCENTAGE);
+            List<String> train = airportValues.subList(0, trainIndex);
+            List<String> test = airportValues.subList(trainIndex + 1, airportValues.size());
+            for(String flight : train) {
+                flightData.add(new FlightData(flight));
+            }
+            LOG.info("training for {}", entry.getKey());
+            OnlineLogisticRegression trainedRegression = onlineRegression(flightData);
+            LOG.info("training completed.");
+            testTrainedRegression(trainedRegression, entry.getKey(), test);
+            regressionMap.put(entry.getKey(), trainedRegression);
+        }
 
         return regressionMap;
+    }
+
+    private static void testTrainedRegression(OnlineLogisticRegression trainedRegression, String key, List<String> testFlights) {
+        Auc eval = new Auc(0.5);
+        for(String flight : testFlights) {
+            FlightData flightData = new FlightData(flight);
+            eval.add(flightData.realResult, trainedRegression.classifyScalar(flightData.vector));
+        }
+        LOG.info("Training accuracy for {} {}", key, eval.auc());
+    }
+
+    private static OnlineLogisticRegression onlineRegression(List<FlightData> allFlightData) {
+        OnlineLogisticRegression logisticRegression = new OnlineLogisticRegression(2, FlightData.NUM_FEATURES, new L1())
+                .learningRate(1)
+                .alpha(1)
+                .lambda(0.000001)
+                .stepOffset(10000)
+                .decayExponent(0.2);
+
+        for(int i=0; i<NUM_EPOCS; i++) {
+            for(FlightData flightData : allFlightData) {
+                logisticRegression.train(flightData.realResult, flightData.vector);
+            }
+        }
+        return logisticRegression;
     }
 
     private static Map<String,List<String>> getRandomSampling(Map<String, List<String>> allData) {
